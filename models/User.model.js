c10dd568ab633bd91ad7a8eb5f93b81dde5c2576
Vanderlei I Martins <vanderlei.i.martins@outlook.com>
2021-02-10 16:27:18 +0000

    iteration 1 done

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,23 @@
 // User model here
+const mongoose = require('mongoose');
+const {Schema, model} = mongoose;
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            trim: true,
+            required: [true, 'Username has to be filled']
+        },
+        password: {
+            type: String,
+            required: [true, 'Please, enter with a valid password']
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = model('User', userSchema);
---
+++ routes/auth.routes.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User.model');
+const bcrypt = require('bcryptjs');
+
+router.get('/signup', (req, res) => {
+    res.render('auth/signup');
+});
+
+router.post('/signup', async (req, res) => {
+    const {username, email, password} = req.body;
+
+    if(username === '' || password === '') {
+        res.render('/signup', {errorMessage: 'username and password must be  filled and valid'});
+
+        return;
+    }
+
+    const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if(! passwordRegex.test(password)) {
+        res.render('auth/signup', {errorMessage: `Your password must contain at least 1 of each: 
+            lowercase letter, uppercase letter, number and have six or more characteres`});
+
+        return;
+    }
+
+    const user = await User.findOne({username: username});
+    if(user) {
+        res.render('auth/signup', {errorMessage: 'Unfortunatly someone already has your username :('});
+
+        return;
+    }
+
+    //Create the user
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    try {
+        await User.create({
+            username,
+            email,
+            password: hashPass
+        });
+
+        res.redirect('/');
+    } catch (err) {
+        if(err.code === 11000) {
+            res.render('auth/signup', {errorMessage: 'Username already registered'});
+        }
+    }
+});
+
+module.exports = router;
